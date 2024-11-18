05391add4c7f8e947d4509cea10988ec100e60a6
muhammadHammadGhazanfar <hammadghazanfaar@gmail.com>
2024-11-18 11:34:18 +0500

    commit

--- Robotronics/vite.config.js
+++ Robotronics/vite.config.js
@@ -5,14 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-<<<<<<< HEAD
     outDir: 'build', // Change the output directory to 'build'
   },
 })
-
-0
-=======
-    outDir: "build", // Set the output directory to 'build' instead of 'dist'
-  },
-});
->>>>>>> 91cad78a0553868a7ef90179558702cea3fca7f2
